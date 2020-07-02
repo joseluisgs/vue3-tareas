1b47d8c31fea877a9868998939e363f229e294f8
joseluisgs <joseluisgonsan@gmail.com>
2020-07-02 11:15:46 +0200

    Tarea Componente

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/tasks',
+    name: 'Task',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Tasks.vue'),
+  },
 ];
 
 const router = new VueRouter({
