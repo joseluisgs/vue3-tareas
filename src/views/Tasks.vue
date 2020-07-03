6e1ef3d1354b3094d30ddbdbf96f094874460cae
joseluisgs <joseluisgonsan@gmail.com>
2020-07-03 10:29:56 +0200

    Objeto Reactive

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -15,7 +15,9 @@ import AppTaskList from '@/components/AppTaskList.vue';
 import AppTaskSearch from '@/components/AppTaskSearch.vue';
 import AppTaskAdd from '@/components/AppTaskAdd.vue';
 // Los elementos qe usaremos
-import { ref, computed } from '@vue/composition-api';
+import {
+  computed, watch, reactive, toRefs,
+} from '@vue/composition-api';
 
 export default {
   name: 'Tasks',
@@ -30,27 +32,53 @@ export default {
   // computed seria nuestros computed
   setup() {
     // Referencias observables. recuerda todo esta en value
-    const tasks = ref(taskList);
-    const search = ref('');
+    // const tasks = ref(taskList);
+    // const search = ref('');
+
+    // usamos la propiedad reactive, que es un proxy reactivo
+    // similar a la función data() de Vue 2
+    // https://vue-composition-api-rfc.netlify.app/api.html#reactive
+    const tasksData = reactive({
+      tasks: taskList,
+      search: '',
+      // eslint-disable-next-line max-len
+      filteredTasks: computed(() => tasksData.tasks.filter((task) => task.title.includes(tasksData.search))),
+    });
 
     // Mi métodos
     function addTask(task) {
-      tasks.value.push({
+      tasksData.tasks.push({
         title: task,
         completed: false,
       });
     }
+    // Convierte un objeto reactivo e un objeto plano
+    // https://vue-composition-api-rfc.netlify.app/api.html#toref
+    const { tasks, search } = toRefs(tasksData);
+    // Si necesitamos actuar cuando un valor se actualiza,
+    // podemos usar la función watch de la Composition API
+    // para llevar el control de forma reactiva de cualquier propiedad.
+    // https://vue-composition-api-rfc.netlify.app/api.html#watch
+    // es interesante respecto a las computed, si hacemos operaciones asíncronas
+    watch(() => {
+      console.log(tasks.value.length);
+    });
+
+    watch(search, (newSearch, oldSearch) => {
+      console.log(`Antes buscabas ${oldSearch} y ahora buscas ${newSearch}`);
+    });
 
     // Campos computados
     // https://vue-composition-api-rfc.netlify.app/api.html#computed
     // podemos retornar un objeto reactivo que actualizará cuando sus dependencias cambien
     // eslint-disable-next-line max-len
-    const filteredTasks = computed(() => tasks.value.filter((task) => task.title.includes(search.value)));
+    // const filteredTasks = computed(() => tasks.value.filter((task) => task.title.includes(search.value)));
 
     // Devolvemos lo que vamos a usar en el componente
-    return {
-      tasks, search, addTask, filteredTasks,
-    };
+    // return {
+    //   tasks, search, addTask, filteredTasks,
+    // };
+    return { ...toRefs(tasksData), addTask };
   },
   // Vue 2
   // created() {
